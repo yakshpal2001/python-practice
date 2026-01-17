6020c1d1d484d1785d241df29705c9362186d901
http://github.com/yakshpal2001/python-practice.git <palyaksh071@gmail.com>
2026-01-17 20:27:47 +0530

    python.py

--- python.py
+++ python.py
@@ -5,6 +5,24 @@
 # else:
 #     # print("cpu usage is normal")
 
-print("starting devops automation..")
-print("checking server status...")
-print("task compleated!")
+# print("starting devops automation..")
+# print("checking server status...")
+# print("task compleated!")
+
+# for i in range (1,6):
+#   print("running backup task" , i)
+
+
+# disk_usage = int(input("enter CPU usge:"))
+ 
+# if disk_usage > 80:
+#     print("CPU is running highb tamptaure")
+# else:
+#     print("CPU is fine")
+
+disk_usage = int(input("enter disk used number:"))
+
+if disk_usage > 70:
+    print("disk almost full")
+else:
+    print("disk is safe")
