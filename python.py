60c17cfde0575cfbffd98ee718c7987b87836a37
https://github.com/yakshpal2001/python-practice.git <palyaksh071@gmail.com>
2026-01-17 12:12:46 +0530

    new code practice

---
+++ python.py
@@ -0,0 +1,10 @@
+# cpu_usage = int(input("enter cpu usage percentage:"))
+
+# if cpu_usage >= 80:
+#     print("alert high cpu usage ")
+# else:
+#     # print("cpu usage is normal")
+
+print("starting devops automation..")
+print("checking server status...")
+print("task compleated!")
