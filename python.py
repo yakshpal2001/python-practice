3ebf7b2cb55af6209ae08cb9a3c1a5dcb1f60cb4
http://github.com/yakshpal2001/python-practice.git <palyaksh071@gmail.com>
2026-01-23 11:44:25 +0530

    new practice code

--- python.py
+++ python.py
@@ -3,7 +3,9 @@
 # if cpu_usage >= 80:
 #     print("alert high cpu usage ")
 # else:
-#     # print("cpu usage is normal")
+#     print("cpu usage is normal")
+
+
 
 # print("starting devops automation..")
 # print("checking server status...")
@@ -13,16 +15,74 @@
 #   print("running backup task" , i)
 
 
+
+
 # disk_usage = int(input("enter CPU usge:"))
- 
-# if disk_usage > 80:
+ # if disk_usage > 80:
 #     print("CPU is running highb tamptaure")
 # else:
 #     print("CPU is fine")
 
-disk_usage = int(input("enter disk used number:"))
 
-if disk_usage > 70:
-    print("disk almost full")
-else:
-    print("disk is safe")
+
+# disk_usage = int(input("enter disk used number:"))
+# if disk_usage > 70:
+#     print("disk almost full")
+# else:
+#     print("disk is safe")
+
+# with open ("log.txt","a") as file:
+#       file.write("this is first server\n ")
+
+# for yaksh in range(1,7):
+#      print("running task", yaksh)
+
+
+# for yaksh in range(1,2):
+#      print("running maintanence task", yaksh)
+
+
+# with open ("system.log", "a") as file:
+#      file.write("system check compleated\n")
+
+
+# names = ["thsi is puthon"]
+
+# for name in names:
+#      print("identifying word:", name)
+
+# names = ["this is python"]
+
+# for names in names:
+#      print("indentifying words:" , names )
+
+# with open("systen.py" , "a") as file:
+#     file.write("this in new repo\n")
+
+# for i in range(22,26):
+#     for j in range(22,26):
+#         print(i,j)
+
+# for i in range(2,7):
+#     for j in range(2,7):
+#         print(i,j)
+
+
+# names = ["Ram", "Shyam", "Aman"]
+# for name in names:
+#     print(name)
+
+# for name in "python":
+#     print(name)
+
+
+
+# i = 1
+# while i < 5:
+#     print(i)
+#     i += 1
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 2
